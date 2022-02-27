6236353e436145182575f288a081962ca81977bb
tatsu0428 <180441115@ccmailg.meijo-u.ac.jp>
2022-02-27 16:42:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def main():
         target = st.sidebar.selectbox("Target", feature_list, index=1)
         #予測対象のデータ個数のカウント
         target_count = df[target].value_counts().reset_index(name="count")
-        print(target_count)
         fig_target = go.Figure(data=[go.Pie(labels=target_count["index"], values=target_count["count"], hole=.3)])
         fig_target.update_layout(showlegend=False, height=200, margin={'l': 20, 'r': 60, 't': 0, 'b': 0})
         fig_target.update_traces(textposition='inside', textinfo='label+percent')
@@ -43,7 +42,7 @@ def main():
         #print(target_count)
 
         #データフレームの表示
-        st.markdown("Data Frame")
+        st.markdown("## Data Frame")
         st.dataframe(df.head())
 
         if is_numeric_dtype(df[feature_selected]):
@@ -55,20 +54,24 @@ def main():
             st.markdown("Missing Value")
             st.text(df[feature_selected].isnull().sum())
         else:
-            #カテゴリカル変数の表示
+            #選択したカテゴリカル変数の個数をカウントし表示
+            cat_count = df.groupby(feature_selected).size().reset_index(name="count")
+            st.dataframe(cat_count)
+
+            #選択したカテゴリカル関数の欠損値の数を表示
+            st.text(df[feature_selected].isnull().sum())
+
+            #選択したカテゴリカル変数の分布のグラフを表示
+            df_cat = df.groupby([feature_selected, target]).size().reset_index(name="count")
             fig_cat = go.Figure()
             for i in target_count["index"]:
-                fig_cat.add_trace(go.Bar(name=str(i), x=df[feature_selected], y=df[df[target]==i]))
-            #fig_cat = go.Figure(data=[go.Bar(x=df[feature_selected], y=df[target])])
+                cat = df_cat[df_cat[target] == i]
+                fig_cat.add_trace(go.Bar(name=str(i), x=cat[feature_selected], y=cat["count"]))
             fig_cat.update_layout(height=300, width=500, margin={'l': 20, 'r': 20, 't': 0, 'b': 0}, legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99), barmode='stack')
             fig_cat.update_xaxes(title_text=None)
             fig_cat.update_yaxes(title_text='# of samples')
+            
             st.plotly_chart(fig_cat, use_container_width=True)
-            st.markdown("")
-
-        #コンテンツのグラフの作成
-        #df_feature = df.groupby(target_name[0])[feature_selected].count().reset_index()
-        #st.markdown(df_feature)
 
 
 if __name__ == "__main__":
